8b0601b80ab39ec3ec6d6b1b2d61701e83342222
artztylj <tyler.artz32@gmail.com>
2022-10-05 12:45:36 -0400

    Completed defensive chart functionality ToDo:   Creation and display of images for ease of reading

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TeamBuilder
 A team builder for Pokemon to better determine both offensive and defensive type coverage.
+
+Currently no outside functionality, all changes to a team must be made in the source code for the time being.
--- TeamBuilder.py
+++ TeamBuilder.py
@@ -22,40 +22,42 @@ def create_team_df(team):
 
     return team_df
 
-def create_chart(team_df):
+#Creates an empty chart for the team with Totals for super effective STAB moves
+def create_offensive_chart(team_df):
     team_size = len(team_df)
+    base_values = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
     if team_size == 6:
-        d = {pokemmo_df.iloc[0].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[1].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[2].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[3].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[4].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[5].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             pokemmo_df.iloc[2].name: base_values,
+             pokemmo_df.iloc[3].name: base_values,
+             pokemmo_df.iloc[4].name: base_values,
+             pokemmo_df.iloc[5].name: base_values,
              'Total': [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]}
     elif team_size == 5:
-        d = {pokemmo_df.iloc[0].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[1].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[2].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[3].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[4].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             pokemmo_df.iloc[2].name: base_values,
+             pokemmo_df.iloc[3].name: base_values,
+             pokemmo_df.iloc[4].name: base_values,
              'Total': [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]}
     elif team_size == 4:
-        d = {pokemmo_df.iloc[0].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[1].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[2].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[3].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             pokemmo_df.iloc[2].name: base_values,
+             pokemmo_df.iloc[3].name: base_values,
              'Total': [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]}
     elif team_size == 3:
-        d = {pokemmo_df.iloc[0].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[1].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[2].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             pokemmo_df.iloc[2].name: base_values,
              'Total': [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]}
     elif team_size == 2:
-        d = {pokemmo_df.iloc[0].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
-             pokemmo_df.iloc[1].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
              'Total': [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]}
     elif team_size == 1:
-        d = {pokemmo_df.iloc[0].name: [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0],
+        d = {pokemmo_df.iloc[0].name: base_values,
              'Total': [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]}
     else:
         print("There was an error with your team size")
@@ -65,17 +67,81 @@ def create_chart(team_df):
                                      'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy'])
     return df
 
-def tally(team_chart, team_df):
+#Creates an empty chart for the team with Resist and Weak columns
+def create_defensive_chart(team_df):
+    team_size = len(team_df)
+    base_values = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+    total_values = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+    if team_size == 6:
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             pokemmo_df.iloc[2].name: base_values,
+             pokemmo_df.iloc[3].name: base_values,
+             pokemmo_df.iloc[4].name: base_values,
+             pokemmo_df.iloc[5].name: base_values,
+             'Weak': total_values,
+             'Resist': total_values}
+    elif team_size == 5:
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             pokemmo_df.iloc[2].name: base_values,
+             pokemmo_df.iloc[3].name: base_values,
+             pokemmo_df.iloc[4].name: base_values,
+             'Weak': total_values,
+             'Resist': total_values}
+    elif team_size == 4:
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             pokemmo_df.iloc[2].name: base_values,
+             pokemmo_df.iloc[3].name: base_values,
+             'Weak': total_values,
+             'Resist': total_values}
+    elif team_size == 3:
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             pokemmo_df.iloc[2].name: base_values,
+             'Weak': total_values,
+             'Resist': total_values}
+    elif team_size == 2:
+        d = {pokemmo_df.iloc[0].name: base_values,
+             pokemmo_df.iloc[1].name: base_values,
+             'Weak': total_values,
+             'Resist': total_values}
+    elif team_size == 1:
+        d = {pokemmo_df.iloc[0].name: base_values,
+             'Weak': total_values,
+             'Resist': total_values}
+    else:
+        print("There was an error with your team size")
+        return 0
+
+    df = pd.DataFrame(data=d, index=['Normal', 'Fire', 'Water', 'Grass', 'Electric', 'Ice', 'Fighting', 'Poison', 'Ground',
+                                     'Flying', 'Psychic', 'Bug', 'Rock', 'Ghost', 'Dragon', 'Dark', 'Steel', 'Fairy'])
+    return df
+
+#Tallies the total super effective STAB typings present in the team
+def tally_offense(team_chart, team_df):
     for types in range(18):
         for pokemon in range(len(team_df)):
             if team_chart.iloc[types,pokemon] > 1.0:
-                team_chart.iloc[types,6] += 1
+                team_chart.iloc[types,(len(team_df))] += 1
 
     return team_chart
 
+#Tallies the total resistances and weaknesses of each type present in the team
+def tally_defense(team_chart, team_df):
+    for types in range(18):
+        for pokemon in range(len(team_df)):
+            if team_chart.iloc[types,pokemon] > 1.0:
+                team_chart.iloc[types,(len(team_df))] += 1
+            elif team_chart.iloc [types,pokemon] < 1.0:
+                team_chart.iloc[types,(len(team_df)+1)] += 1
+    return team_chart
+
+#Creates an offensive chart for the specified team according to STAB moves
 def calc_offense(team_df):
     #Creates an empty chart for the Team
-    team_chart = create_chart(team_df)
+    team_chart = create_offensive_chart(team_df)
 
     #Loops through each pokemon
     for pokemon in range(len(team_df)):
@@ -101,14 +167,40 @@ def calc_offense(team_df):
                 if (type_effectiveness[types2]) > team_chart.iloc[types2,pokemon]:
                     team_chart.iloc[types2,pokemon] = type_effectiveness[types2]
     
-    final_off_chart = tally(team_chart, team_df)
+    final_off_chart = tally_offense(team_chart, team_df)
 
     return final_off_chart
 
+#Creates a defensive chart for the specified team
 def calc_defense(team_df):
-    create_chart(team_df)
+    #Creates an empty chart for the team_df
+    team_chart = create_defensive_chart(team_df)
 
-    return 0
+    #Loops through each pokemon
+    for pokemon in range(len(team_df)):
+        #Checks how many types the current pokemon has (1 or 2)
+        # and creates an object with only the type(s)
+        type2 = team_df.iloc[pokemon].get(['Type 2'])
+        if type2.isna().values[0]:
+            type = team_df.iloc[pokemon].get(['Type 1']).values
+        else:
+            type = team_df.iloc[pokemon].get(['Type 1', 'Type 2']).values
+
+        #Loops through each type in the chart for the current
+        # pokemon and sets it to the correct value based on the type chart
+        type_effectiveness = type_chart.loc[:,type[0]].values
+        for types in range(18):
+            team_chart.iloc[types,pokemon] = type_effectiveness[types] * team_chart.iloc[:,pokemon].values[types]
+
+        #Checks for a second typing and multiplies the values
+        # together to create the correct value
+        if len(type) == 2:
+            type_effectiveness = type_chart.loc[:,type[1]].values
+            for types2 in range(18):
+                team_chart.iloc[types2,pokemon] = type_effectiveness[types2] * team_chart.iloc[:,pokemon].values[types2]
+        
+    final_def_chart = tally_defense(team_chart, team_df)
+    return final_def_chart
 
 dataset = "pokemon.csv"
 pokedex = pd.read_csv(dataset)
@@ -118,10 +210,13 @@ chart = "chart.csv"
 type_chart = pd.read_csv(chart)
 type_chart.set_index("Attacking", inplace=True)
 
-pokemmo_team = ["Torterra", "Alakazam", "Crobat", "Scizor", "Magnezone", "Kingdra"]
+pokemmo_team = ["Torterra", "Crobat", "Magnezone", "Umbreon", "Alakazam", "Kingdra"]
 pokemmo_df = create_team_df(pokemmo_team)
 
 offensive_chart = calc_offense(pokemmo_df)
-
+defensive_chart = calc_defense(pokemmo_df)
 print('                             OFFENSIVE CHART')
 print(offensive_chart)
+
+print('                             DEFENSIVE CHART')
+print(defensive_chart)
